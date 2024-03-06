1ebfc6ce99f21f9a462e9b17011bb4ae187f4674
Miro531 <popov.miroslav@hotmail.it>
2024-03-06 09:19:52 +0100

    Day 22: Render the app in titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -9,6 +10,25 @@ with col1:
 with col2:
     st.title("Miro")
     content = """
-    Hi there everyone my name is Miro I'am an adveturist an tech entusiast
+    Hi there everyone my name is Miro I'am an adveturist an tech entusiast based in Italy. Currently I'am deeping in my knowledge 
+    in Python, Red hat administration and AWS in order to get certified. This site is a showcase of some applications and web applications I created as
+    a portafolio.
     """
-    st.write(content)
+    st.info(content)
+
+content2 = """
+   some random text 
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
